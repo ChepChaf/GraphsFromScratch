ec0d5071949f81522e0d9fe079a71b667e7c044e
Cezar Azevedo <cazevedo@wilab.io>
2021-09-02 23:37:19 -0300

    Update gitignore

--- cmake-build-debug/conan_paths.cmake
+++
@@ -1,8 +0,0 @@
-set(CONAN_GLM_ROOT "/home/chep/.conan/data/glm/0.9.9.8/_/_/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9")
-set(CONAN_YAML-CPP_ROOT "/home/chep/.conan/data/yaml-cpp/0.7.0/_/_/package/b911f48570f9bb2902d9e83b2b9ebf9d376c8c56")
-set(CMAKE_MODULE_PATH "/home/chep/.conan/data/glm/0.9.9.8/_/_/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/"
-			"/home/chep/.conan/data/yaml-cpp/0.7.0/_/_/package/b911f48570f9bb2902d9e83b2b9ebf9d376c8c56/"
-			"/home/chep/.conan/data/yaml-cpp/0.7.0/_/_/package/b911f48570f9bb2902d9e83b2b9ebf9d376c8c56/lib/cmake" ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
-set(CMAKE_PREFIX_PATH "/home/chep/.conan/data/glm/0.9.9.8/_/_/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/"
-			"/home/chep/.conan/data/yaml-cpp/0.7.0/_/_/package/b911f48570f9bb2902d9e83b2b9ebf9d376c8c56/"
-			"/home/chep/.conan/data/yaml-cpp/0.7.0/_/_/package/b911f48570f9bb2902d9e83b2b9ebf9d376c8c56/lib/cmake" ${CMAKE_PREFIX_PATH} ${CMAKE_CURRENT_LIST_DIR})
